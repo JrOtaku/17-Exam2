075d67f15b8f6758b770c625c15555966f71c1fd
JrOtaku <emilya200.ea@gmail.com>
2019-01-15 21:08:26 -0500

    Here is my test 2! Thank you!

--- src/problem2.py
+++ src/problem2.py
@@ -3,8 +3,8 @@ Exam 2, problem 2.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Emily Guajardo.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import math
 import time
@@ -13,7 +13,7 @@ import testing_helper
 
 
 ###############################################################################
-# TODO: 2.  READ the   Point   class defined below.
+# Done: 2.  READ the   Point   class defined below.
 #  Note especially its methods:
 #    clone
 #    distance_from
@@ -87,12 +87,12 @@ def main():
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
 
-    # run_test_init()
-    # run_test_area()
-    # run_test_bigger_triangle()
-    # run_test_shrink_or_expand()
-    # run_test_return_doubled_triangle()
-    # run_test_get_largest_area()
+    run_test_init()
+    run_test_area()
+    run_test_bigger_triangle()
+    run_test_shrink_or_expand()
+    run_test_return_doubled_triangle()
+    run_test_get_largest_area()
 
 
 ###############################################################################
@@ -137,8 +137,12 @@ class Triangle(object):
           :type b: Point
           :type c: Point
         """
+        self.a = Point.clone(a)
+        self.b = Point.clone(b)
+        self.c = Point.clone(c)
+        self.history = []
         # ---------------------------------------------------------------------
-        # TODO: 2.
+        # Done: 2.
         #   a. READ the above specification, including the Example.
         #        ** ASK QUESTIONS AS NEEDED. **
         #        ** Be sure you understand it, ESPECIALLY the Example.
@@ -177,8 +181,14 @@ class Triangle(object):
         Type hints:
           :rtype: float
         """
+        side1 = self.a.distance_from(self.b)
+        side2 = self.b.distance_from(self.c)
+        side3 = self.c.distance_from(self.a)
+        s = .5 * (side1 + side2 + side3)
+        area = math.sqrt(s * (s - side1) * (s - side2) * (s - side3))
+        return area
         # ---------------------------------------------------------------------
-        # TODO: 3.
+        # Done: 3.
         #   a. READ the above specification, including the Example AND HINT!
         #        ** ASK QUESTIONS AS NEEDED. **
         #        ** Be sure you understand it, ESPECIALLY the Example.
@@ -200,8 +210,12 @@ class Triangle(object):
           :type: triangle2: Triangle
           :rtype: bool
         """
+        if self.area() > triangle2.area():
+            return True
+        else:
+            return False
         # ---------------------------------------------------------------------
-        # TODO 4:
+        # Done 4:
         #   a. READ the above specification, including the Example.
         #        ** ASK QUESTIONS AS NEEDED. **
         #        ** Be sure you understand it, ESPECIALLY the Example.
@@ -223,8 +237,18 @@ class Triangle(object):
          Type hints:
            :type: f: float
         """
+        self.a.x *= f
+        self.b.x *= f
+        self.c.x *= f
+        self.a.y *= f
+        self.b.y *= f
+        self.c.y *= f
+        self.history += [self.area()]
+        # self.history += [Triangle(Point(self.a.x*f, self.a.y*f),
+        #                           Point(self.b.x*f, self.b.y*f),
+        #                           Point(self.c.x*f, self.c.y*f)).area()]
         # ---------------------------------------------------------------------
-        # TODO 8:
+        # Done 8:
         #   a. READ the above specification, including the Example.
         #        ** ASK QUESTIONS AS NEEDED. **
         #        ** Be sure you understand it, ESPECIALLY the Example.
@@ -246,8 +270,12 @@ class Triangle(object):
         Type hints:
           :rtype: Triangle:
         """
+        a = Point(self.a.x*2, self.a.y*2)
+        b = Point(self.b.x*2, self.b.y*2)
+        c = Point(self.c.x*2, self.c.y*2)
+        return Triangle(a, b, c)
         # -------------------------------------------------------------------------
-        # TODO: 9
+        # Done: 9
         #   a. READ the above specification, including the Example.
         #        ** ASK QUESTIONS AS NEEDED. **
         #        ** Be sure you understand it, ESPECIALLY the Example.
@@ -270,8 +298,16 @@ class Triangle(object):
         Type hints:
           :rtype: Float:
         """
+        count = 0
+        if len(self.history) > 0:
+            for k in range(len(self.history)):
+                if count < self.history[k]:
+                    count = self.history[k]
+        else:
+            count = self.area()
+        return count
         # ---------------------------------------------------------------------
-        # TODO: 9
+        # Done: 9
         #   a. READ the above specification, including the Example.
         #        ** ASK QUESTIONS AS NEEDED. **
         #        ** Be sure you understand it, ESPECIALLY the Example.
